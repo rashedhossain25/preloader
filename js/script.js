43e2497b12a117e98b3489d6a969cf820361b65b
Rashed Hossain <145587722+rashedhossain25@users.noreply.github.com>
2024-01-03 10:11:52 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+//jq code
+
+$(document).ready(function(){
+    //PREELOADER
+    $('.js-preloader').preloadinator({
+        minTime: 3000
+   
+   });
+
+// $(selector).countMe(delay,speed)
+});
